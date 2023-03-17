a9415bb4584930f9f6a2cb7eb321c0fd0ce6c07e
SoukaJoud <soukaina.ezzaoui@gmail.com>
2023-03-17 03:10:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int main(void){
-  char str[]="Programming is like building a multilingual puzzle";
-  puts(str);
-  return 0;
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main ()
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
