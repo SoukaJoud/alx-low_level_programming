e1ec0f96a305d2ff97120d9102af77307e041b56
SoukaJoud <soukaina.ezzaoui@gmail.com>
2023-03-17 03:13:37 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  * Return: Always 0 (Success)
  */
 
-int main ()
+int main (void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts ("\"Programing is like building a multilingual puzzle");
+return (0);
 }
